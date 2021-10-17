a670caf7324a6c9372053c2b518f98a00cbffe94
yjiangck <yjiangck@connect.ust.hk>
2021-10-17 21:47:31 +0800

    Lab5 done

--- Labs/Lab5/Planet.cpp
+++ Labs/Lab5/Planet.cpp
@@ -13,9 +13,34 @@ using namespace std;
 * by setting 'activated' to false directly. Set flag_deactivate to true, and it will be deactivated
 * later in your do_single_iteration function. 
 */
-void Planet::determine_collision() {
+void Planet::determine_collision()
+{
 	// START OF YOUR IMPLEMENTATION
-	
+	if (activated)
+	{
+		for (int j = uid + 1; j < MAX_PLANETS; j++)
+		{
+			if (planets[j].activated)
+			{
+				if (std::hypot((x - planets[j].x), (y - planets[j].y)) < 0.1)
+				{
+					if (mass == planets[j].mass)
+					{
+						flag_deactivate = true;
+						planets[j].flag_deactivate = true;
+					}
+					else if (mass > planets[j].mass)
+					{
+						planets[j].flag_deactivate = true;
+					}
+					else if (mass < planets[j].mass)
+					{
+						flag_deactivate = true;
+					}
+				}
+			}
+		}
+	}
 	// END OF YOUR IMPLEMENTATION
 }
 
@@ -23,28 +48,54 @@ void Planet::determine_collision() {
 * Updates the acceleration of the planet. Note that this is just the sum of forces divided by the mass.
 * Use the formula that is stated in the webpage.
 */
-void Planet::update_acceleration() {
+void Planet::update_acceleration()
+{
 	// START OF YOUR IMPLEMENTATION
-	
+	if (activated)
+	{
+		ax = 0;
+		ay = 0;
+		for (int j = 0; j < MAX_PLANETS; j++)
+		{
+			if (planets[j].activated && j != uid)
+			{
+				double dx = planets[j].x - x;
+				double dy = planets[j].y - y;
+				double r = std::hypot(dx, dy);
+				ax += planets[j].mass * dx / (r * r * r);
+				ay += planets[j].mass * dy / (r * r * r);
+			}
+		}
+	}
+
 	// END OF YOUR IMPLEMENTATION
 }
-
 /**
 * Updates the velocity of the planet. In one second, the main loop runs FPS times, and in each call of the main loop, STEPS 
 * iterations will be called. This means there should be approximately STEPS * FPS iterations per second, and therefore the
 * change in velocity should approximately be the acceleration divided by (STEPS * FPS)
 */
-void Planet::update_velocity() {
+void Planet::update_velocity()
+{
 	// START OF YOUR IMPLEMENTATION
-	
+	if (activated)
+	{
+		vx += ax / (STEPS * FPS);
+		vy += ay / (STEPS * FPS);
+	}
 	// END OF YOUR IMPLEMENTATION
 }
 
 /**
 * Similarly, the change in displacement is approximately the velocity divided by (STEPS * FPS)
 */
-void Planet::update_displacement() {
+void Planet::update_displacement()
+{
 	// START OF YOUR IMPLEMENTATION
-	
+	if (activated)
+	{
+		x += vx / (STEPS * FPS);
+		y += vy / (STEPS * FPS);
+	}
 	// END OF YOUR IMPLEMENTATION
 }
--- Labs/Lab5/Simulation.cpp
+++ Labs/Lab5/Simulation.cpp
@@ -3,8 +3,10 @@
 /**
 * Deactivates all the planets, which are flagged to be deactivated
 */
-void commit_activations() {
-	for (int i = 0; i < MAX_PLANETS; ++i) {
+void commit_activations()
+{
+	for (int i = 0; i < MAX_PLANETS; ++i)
+	{
 		planets[i].activated = planets[i].activated & !planets[i].flag_deactivate;
 	}
 }
@@ -13,22 +15,36 @@ void commit_activations() {
 * Runs a single iteration of the simulation of the planets.
 * Only call the respective functions on activated planets
 */
-void do_single_iteration() {
+void do_single_iteration()
+{
 	// START OF YOUR IMPLEMENTATION
 
 	// First check the collisions of the planets
+	for (int i = 0; i < MAX_PLANETS; i++)
+	{
+		planets[i].determine_collision();
+	}
 
 	// Deactivate all the planets that are flagged to be removed from the previous methods
-	
+	commit_activations();
 
 	// Update the acceleration
-	
+	for (int i = 0; i < MAX_PLANETS; i++)
+	{
+		planets[i].update_acceleration();
+	}
 
 	// Update the velocity
-	
+	for (int i = 0; i < MAX_PLANETS; i++)
+	{
+		planets[i].update_velocity();
+	}
 
 	// Update the displacement
-	
-	
+	for (int i = 0; i < MAX_PLANETS; i++)
+	{
+		planets[i].update_displacement();
+	}
+
 	// END OF YOUR IMPLEMENTATION
 }
